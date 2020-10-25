7baf26418d3d1fc0d2adead2ed567a2b4e062a11
Tasmeem-Hossain <73355609+Tasmeem-Hossain@users.noreply.github.com>
2020-10-25 22:26:04 +0600

    new code

---
+++ odd.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int x,sum,i;
+    int count=0;
+    printf("Enter a number:");
+    scanf("%d",&x);
+
+
+    for(i=0;i<=100;i++)
+    {
+        printf();
+    }
+
+
+    return 0;
+}
